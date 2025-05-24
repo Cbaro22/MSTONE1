ed346be79b182f7a3f1e5a16bad85e4dc4a9119b
Clerk <kobrynclerk@gmail.com>
2025-05-24 15:29:19 +0100

    added 2 new files

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bcryptjs = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const User = require("./userModel")
 const Product = require("./productModel")
+const forgotPasswordMail = require("./sendMail")
 dotenv.config()
 
 app.use(express.json())
@@ -101,6 +102,48 @@ app.post('/login', async (req, resp) =>{
         }
 
     })
+})
+
+app.post('/forgot-password', async (req, resp) =>{
+    const {email} = req.body;
+
+    const user = await User.findOne({email})
+
+    if(!user){
+        return resp.status(404).json({message:"User account not found"})
+    }
+
+    accessToken = await jwt.sign(
+        {user},
+        `${process.env.ACCESS_TOKEN}`,
+        {expiresIn: "5m"}
+
+    )
+
+    await forgotPasswordMail(email, accessToken)
+    //send OTP
+
+    resp.status(200).json({
+        message:"Please check your email"
+    })
+})
+
+app.patch('/reset-password', async (req, resp) =>{
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+
+    if(!user){
+        return resp.status(404).json({message:"user account not found"})
+    }
+
+    const hashedPassword =await bcrypt.hash(password, 12)
+
+    user.password = hashedPassword
+
+    await user.save()
+     
+    resp.status(200).json({message:"password reset successful"})
 })
 
 app.get('/get-products', async (req, resp)=>{
